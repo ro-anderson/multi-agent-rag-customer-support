a25715c6c023fbe08644b7a712abdf46854a31c6
ro-anderson <didier.rda@gmail.com>
2024-09-13 00:01:54 -0300

    chore: add __init__ with imports

---
+++ customer_support_chat/app/services/tools/__init__.py
@@ -0,0 +1,47 @@
+# customer_support_chat/app/services/tools/__init__.py
+
+from .lookup import lookup_policy
+from .flights import (
+    fetch_user_flight_information,
+    search_flights,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+)
+from .cars import (
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+)
+from .hotels import (
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+)
+from .excursions import (
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+)
+
+__all__ = [
+    "lookup_policy",
+    "fetch_user_flight_information",
+    "search_flights",
+    "update_ticket_to_new_flight",
+    "cancel_ticket",
+    "search_car_rentals",
+    "book_car_rental",
+    "update_car_rental",
+    "cancel_car_rental",
+    "search_hotels",
+    "book_hotel",
+    "update_hotel",
+    "cancel_hotel",
+    "search_trip_recommendations",
+    "book_excursion",
+    "update_excursion",
+    "cancel_excursion",
+]
