542741aa61c5d25daea0912fee332fb76e7da70d
ro-anderson <didier.rda@gmail.com>
2024-09-13 00:02:09 -0300

    chore: remove unused files

--- customer_support_chat/app/models/conversation.py
+++
@@ -1,9 +0,0 @@
-# customer_support_chat/app/models/conversation.py
-
-class Conversation:
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.history = []
-
-    def add_message(self, message):
-        self.history.append(message)
