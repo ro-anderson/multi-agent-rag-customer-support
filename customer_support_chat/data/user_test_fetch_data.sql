d506958b47439abc402388510513e287f894b1ec
ro-anderson <didier.rda@gmail.com>
2024-09-18 05:42:18 -0300

    chore: add test query

---
+++ customer_support_chat/data/user_test_fetch_data.sql
@@ -0,0 +1,11 @@
+SELECT
+    t.ticket_no, t.book_ref,
+    f.flight_id, f.flight_no, f.departure_airport, f.arrival_airport, f.scheduled_departure, f.scheduled_arrival,
+    bp.seat_no, tf.fare_conditions
+FROM 
+    tickets t
+    JOIN ticket_flights tf ON t.ticket_no = tf.ticket_no
+    JOIN flights f ON tf.flight_id = f.flight_id
+    LEFT JOIN boarding_passes bp ON bp.ticket_no = t.ticket_no AND bp.flight_id = f.flight_id
+WHERE 
+    t.passenger_id = '8149 604011'; 
